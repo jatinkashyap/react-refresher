1e8cbea2cee3a8013172bbb9d32300c1f34b2f6d
jatinkashyap <jatinkashyap9@gmail.com>
2025-08-18 09:56:31 -0400

    Added more components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,28 +1,12 @@
 
-import reactImg from './assets/react-core-concepts.png';
+
 import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
-import CoreConcept from './CoreConcept.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} Fundamental React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
 
 
 
@@ -36,12 +20,21 @@ function App() {
           <ul>
             <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={componentsImg}/>
             <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+            <TabButton>Events</TabButton>
+          </menu>
+
+        </section>
       </main>
     </div>
   );
